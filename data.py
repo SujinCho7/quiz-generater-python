437252e7a2e819e4e03625c4958a2536f8e5673e
SJSJC <flyflyinu@gmail.com>
2024-08-23 12:16:35 +0900

    upload files

---
+++ data.py
@@ -0,0 +1,12 @@
+question_data = [
+    {"text": "Hello = こんにちは", "answer": "True"},
+    {"text": "Meal = お金 : おかね", "answer": "False"},
+    {"text": "Good morning = おはようございます.", "answer": "True"},
+    {"text": "Lunch = ランチ", "answer": "True"},
+    {"text": "Friend = 友達 : ともだち、", "answer": "True"},
+    {"text": "Order = メニュー", "answer": "False"},
+    {"text": "Money = お金 : おかね", "answer": "True"},
+    {"text": "Hotel = 家 : いえ", "answer": "False"},
+    {"text": "Name = 名前 : なまえ", "answer": "True"},
+    {"text": "Time = 時間 : じかん", "answer": "True"},
+]
